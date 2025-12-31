5599d79d4f1fdbb350325b48d1878de79009e0f9
Joao Gabriel <joaogabriel443@gmail.com>
2025-12-31 09:33:01 -0300

    Automated commit 2025-12-31 09:33:01

---
+++ src/test/java/br/com/joaonevesdev/fuelflow/api/repositories/AddressRepositoryIT.java
@@ -0,0 +1,81 @@
+package br.com.joaonevesdev.fuelflow.api.repositories;
+
+import br.com.joaonevesdev.fuelflow.api.TestcontainersConfiguration;
+import br.com.joaonevesdev.fuelflow.api.model.entity.Address;
+import br.com.joaonevesdev.fuelflow.api.model.entity.FuelPrice;
+import br.com.joaonevesdev.fuelflow.api.model.entity.FuelStation;
+import br.com.joaonevesdev.fuelflow.api.repository.AddressRepository;
+import br.com.joaonevesdev.fuelflow.api.repository.FuelPriceRepository;
+import br.com.joaonevesdev.fuelflow.api.repository.FuelStationRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Import(TestcontainersConfiguration.class)
+@ActiveProfiles("test")
+public class AddressRepositoryIT {
+
+
+    @Autowired
+    FuelStationRepository fuelStationRepository;
+    @Autowired
+    AddressRepository addressRepository;
+    @Autowired
+    FuelPriceRepository fuelPriceRepository;
+
+    private Long addressId;
+    private final String street = "Rua 1";
+    private final String neighborhood = "centro";
+    private final String municipality = "araraquara";
+    private final String state = "SP";
+
+    @BeforeEach
+    void limpaBanco() {
+        fuelPriceRepository.deleteAll();
+        fuelStationRepository.deleteAll();
+        addressRepository.deleteAll();
+    }
+
+    @BeforeEach
+    void setup() {
+        LocalDateTime now = LocalDateTime.now();
+        Address a = new Address(street, neighborhood, municipality, state, "sudeste", "123456789", 0.0, 0.0, now, now);
+        a = addressRepository.save(a);
+        addressId = a.getId();
+        FuelStation fs = new FuelStation("99999999999999", a, "1", "rua do campo", "posto de teste", "branca", "posto do teste ltda", true, now, now);
+        fuelStationRepository.save(fs);
+
+        FuelPrice fp = new FuelPrice(fs, LocalDate.of(2025, 12, 30), "etanol", BigDecimal.valueOf(3.89), null, "R$/L", now);
+        fuelPriceRepository.save(fp);
+    }
+
+    @Test
+    @Transactional
+    void findByCompleteAddress() {
+        Optional<Address> res = addressRepository.findByCompleteAddress(street, neighborhood, municipality, state);
+
+        assertFalse(res.isEmpty());
+        assertNotNull(res.get());
+        assertEquals("123456789", res.get().getCep());
+    }
+
+    @Test
+    @Transactional
+    void findByCompleteAddressNotExists() {
+        Optional<Address> res = addressRepository.findByCompleteAddress("street", neighborhood, municipality, state);
+
+        assertTrue(res.isEmpty());
+    }
+}
